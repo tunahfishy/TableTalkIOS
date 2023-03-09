dac0c6d5e7813841966ea5cde7eaf36b930b8003
Tinah <tinahh@mit.edu>
2023-03-09 16:43:10 -0500

    logout

--- pages/ProfileScreen.tsx
+++ pages/ProfileScreen.tsx
@@ -7,15 +7,24 @@ import {
   View,
   Text,
   StyleSheet,
+  TouchableOpacity,
 } from "react-native";
 import { AuthContext } from "../navigation/AuthNavigator";
-import { db } from "../util/firebase";
+import { auth, db } from "../util/firebase";
 import getUser from "../util/getUser";
 
 export default function FriendsScreen({}) {
   const [refreshing, setRefreshing] = useState<boolean>(false);
   const user = useContext(AuthContext);
   const [userData, setUserData] = useState<UserInfo>();
+
+  async function logOut() {
+    try {
+      await auth.signOut();
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   const onRefresh = () => {
     setRefreshing(true);
@@ -45,6 +54,9 @@ export default function FriendsScreen({}) {
     >
       <View style={styles.container}>
         <Text style={styles.profile}>{userData?.name}</Text>
+        <TouchableOpacity style={styles.button} onPress={logOut}>
+          <Text style={styles.buttonText}>Sign out 🤷</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -63,5 +75,21 @@ const styles = StyleSheet.create({
   },
   profile: {
     fontSize: 24,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 12,
+    marginTop: 20,
+    width: 150,
+    borderRadius: 32,
+    elevation: 3,
+    backgroundColor: "#583C2C",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 14,
+    // marginBottom: 10,
+    // marginTop: 10,
   },
 });
